224c1708c93d85185f0c19b5f5220818a9a93731
javierBorjas <cristman11@gmail.com>
2024-06-17 20:29:15 -0300

    El Método Mount de Livewire

--- app/Livewire/EditarVacante.php
+++ app/Livewire/EditarVacante.php
@@ -3,11 +3,19 @@
 namespace App\Livewire;
 
 use App\Models\Salario;
+use App\Models\Vacante;
 use Livewire\Component;
 use App\Models\Categoria;
 
 class EditarVacante extends Component
 {
+    public $titulo;
+
+    public function mount(Vacante $vacante)
+    {
+        $this->titulo = $vacante->titulo;
+    }
+
     public function render()
     {
          // Consulta BD
