79847bc742e360e29f9405c4dbf4c0e83f1aaf50
javierBorjas <cristman11@gmail.com>
2024-06-18 02:47:28 -0300

    Validación del Formulario

--- resources/views/livewire/postular-vacante.blade.php
+++ resources/views/livewire/postular-vacante.blade.php
@@ -1,11 +1,14 @@
 <div class="bg-gray-100 p-5 mt-10 flex flex-col justify-center items-center">
     <h3 class="text-center text-2xl font-bold my-4">Postularme a esta Vacante</h3>
 
-    <form class="w-96 mt-5">
+    <form class="w-96 mt-5" wire:submit.prevent='postularme'>
         <div class="mb-4">
             <x-input-label for="cv" :value="__('Curriculum o Hoja de Vida (PDF)')" />
-            <x-text-input id="cv" type="file" accept=".pdf" class="block mt-1 w-full" />
+
+            <x-text-input id="cv" type="file" wire:model="cv" accept=".pdf" class="block mt-1 w-full" />
         </div>
+
+        <x-input-error :messages="$errors->get('cv')" class="mt-2 uppercase" />
 
         <x-primary-button class="my-5">
             {{__('Postularme')}}
