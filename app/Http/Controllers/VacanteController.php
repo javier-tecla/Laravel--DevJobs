33cbb11f7088eb9f9a13837e62877118b4e7360d
javierBorjas <cristman11@gmail.com>
2024-06-18 02:01:59 -0300

    Ocultar Acceso a Devs para Listar Vacantes

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -13,6 +13,7 @@ class VacanteController extends Controller
     public function index()
     {
         //
+        $this->authorize('viewAny', Vacante::class);
         return view('vacantes.index');
     }
 
