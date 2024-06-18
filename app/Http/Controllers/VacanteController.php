479f806e2662ea8ad5e625d3669f523c86fe777b
javierBorjas <cristman11@gmail.com>
2024-06-17 23:33:09 -0300

    Añadiendo un Policy de Edición

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -47,6 +47,7 @@ class VacanteController extends Controller
     public function edit(Vacante $vacante)
     {
         //
+        $this->authorize('update', $vacante);
 
         return view('vacantes.edit', [
             'vacante' => $vacante
