b43becc77c1e330e2d61c9027787d4a9d052e97b
javierBorjas <cristman11@gmail.com>
2024-06-18 21:12:27 -0300

    Creando la página principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
+            
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white shadow">
